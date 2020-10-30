cb402e9f59e60f49ee009c504c80087593c1f1b6
Matvey <70859084+Ferum-bot@users.noreply.github.com>
2020-10-30 21:31:51 +0300

    done first design

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.calculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
